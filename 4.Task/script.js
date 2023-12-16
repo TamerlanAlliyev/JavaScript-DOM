04198735c94f9be88966fed88fadde5ae8d29dec
Tamerlan Aliyev <146847336+TamerlanAlliyev@users.noreply.github.com>
2023-12-16 18:54:43 +0400

    Update script.js

--- 4.Task/script.js
+++ 4.Task/script.js
@@ -1,35 +1,31 @@
-
-
 "use strict";
 
-const increaseBtn = document.querySelector(".increase");
-const decreaseBtn = document.querySelector(".decrease");
-const counterEl = document.querySelector(".counter");
+const increaseButton = document.querySelector(".increase");
+const decreaseButton = document.querySelector(".decrease");
+const countText = document.querySelector(".counter");
 
-let counter = 0;
+let countValue = 0;
 
+increaseButton.addEventListener("click", () => {
+  countValue++;
+  countText.textContent = Math.abs(countValue);
+  if(countValue > 0){
+    countText.style.color = "green";
+  }else if(countValue ==0){
+    countText.style.color = "white";
+  }else{
+    countText.style.color = "red";
+  }
+});
 
-
-increaseBtn.addEventListener("click", () => {
-    counter++;
-
-    if (counter > 0) {
-        counterEl.style.color = "green";
-    } else if (counter === 0) {
-        counterEl.style.color = "white";
-    }
-
-    counterEl.textContent = Math.abs(counter);
-})
-
-decreaseBtn.addEventListener("click", () => {
-    counter--;
-
-    if (counter < 0) {
-        counterEl.style.color = "red";
-    } else if (counter === 0) {
-        counterEl.style.color = "white";
-    }
-
-    counterEl.textContent = Math.abs(counter);
-})
+decreaseButton.addEventListener("click", () => {
+  countValue--;
+  countText.textContent = Math.abs(countValue);
+  if(countValue > 0){
+    countText.style.color = "green";
+  }else if(countValue ==0){
+    countText.style.color = "white";
+  }else{
+    countText.style.color = "red";
+  }
+});
