7593f98ecbba91f8bdbdb36cd8df911db1c0482a
TamerlanAlliyev <tamerlanma@code.edu.az>
2024-01-02 16:28:44 +0400

    finshed

--- 1.Task/script.js
+++ 1.Task/script.js
@@ -1,44 +1,25 @@
-/*
-const boxEl = document.querySelector(".top-box");
-const btboxEl = document.querySelector(".bottom-box");
-const imgEl = document.querySelector(".icon img");
 
-let isClicked = false;
+const boxes = document.querySelectorAll(".top-box");
+const bottomBoxes = document.querySelectorAll(".bottom-box");
+const icons = document.querySelectorAll(".icon img");
 
-boxEl.addEventListener("click", () => {
-  if (!isClicked) {
-    btboxEl.style.display = "flex";
-    boxEl.style.borderRadius = "5px 5px 0 0";
-    imgEl.src = "./assets/icons/subtraction-svgrepo-com (1).svg"; 
-    isClicked = true;
-  } else {
-    btboxEl.style.display = "none";
-    boxEl.style.borderRadius = "5px";
-    imgEl.src = "./assets/icons/plus-large-svgrepo-com (1).svg";
-    isClicked = false;
-  }
-});
+let lastIndex = null;
 
-*/
+boxes.forEach((box, index) => {
+  box.addEventListener("click", () => {
+    if (lastIndex !== null && lastIndex !== index) {
+      bottomBoxes[lastIndex].classList.remove("active");
+      icons[lastIndex].src = "./assets/icons/plus-large-svgrepo-com (1).svg";
+    }
 
-const boxEl = document.querySelectorAll(".top-box");
-const btboxEl = document.querySelectorAll(".bottom-box");
-const imgEl = document.querySelectorAll(".icon img");
-
-let isClicked = false;
-
-boxEl.forEach((element, index) => {
-  element.addEventListener("click", () => {
-    if (!isClicked) {
-      btboxEl[index].style.display = "flex";
-      boxEl[index].style.borderRadius = "5px 5px 0 0";
-      imgEl[index].src = "./assets/icons/subtraction-svgrepo-com (1).svg"; 
-      isClicked = true;
+    if (bottomBoxes[index].classList.contains("active")) {
+      bottomBoxes[index].classList.remove("active");
+      icons[index].src = "./assets/icons/plus-large-svgrepo-com (1).svg";
+      lastIndex = null;
     } else {
-      btboxEl[index].style.display = "none";
-      boxEl[index].style.borderRadius = "5px";
-      imgEl[index].src = "./assets/icons/plus-large-svgrepo-com (1).svg";
-      isClicked = false;
+      bottomBoxes[index].classList.add("active");
+      icons[index].src = "./assets/icons/subtraction-svgrepo-com (1).svg";
+      lastIndex = index;
     }
   });
 });
